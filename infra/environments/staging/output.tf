9b38c2d5234d230a2d551df19aebce376c886c1f
orlyplaza <plaza.orly.omeles@gmail.com>
2025-11-12 01:07:33 +0800

    feat: add cloudfont distribution id as output

--- infra/environments/staging/output.tf
+++ infra/environments/staging/output.tf
@@ -3,6 +3,11 @@ output "cloudfront_domain_name" {
   value       = module.cloudfront.domain_name
 }
 
+output "cloudfront_distribution_id" {
+  description = "Cloudfront distribution Id"
+  value = module.cloudfront.distribution_id
+}
+
 output "s3_bucket_name" {
   description = "Deployed S3 bucket name"
   value       = module.s3_bucket.bucket_name
